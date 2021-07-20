7bbf36e6c1ef2cd95b397b860a7bd5c7209f4676
zzczzc20 <62371873+zzczzc20@users.noreply.github.com>
2021-07-21 05:10:47 +0800

    Initial construction of SV display

---
+++ SV.java
@@ -0,0 +1,83 @@
+import java.util.*;
+public class SV {
+    public static void main(String args[]){
+        System.out.println("This is the display program for FastSV");
+    }
+}
+
+class SVKernel {
+    private int TotOperations;
+    private int OperationsInParallel;
+    private ArrayList<Integer> FatherVector;
+    private ArrayList<Integer> NextFatherVector;
+    private ArrayList<Integer> GrandFatherVector;
+    private ArrayList<Integer> EdgeStartPoint;
+    private ArrayList<Integer> EdgeEndPoint;
+    private boolean compareTwoArrayList(ArrayList<Integer> a, ArrayList<Integer> b) {
+        boolean ret = true;
+        if(a.size() != b.size()) return false;
+        for (int i = 0; i < a.size(); i++){
+            if(a.get(i) != b.get(i)) return false;
+        }
+        return ret;
+    }
+    private void updateFatherVector(){
+        for (int i = 0; i < FatherVector.size(); i++){
+            FatherVector.set(i, NextFatherVector.get(i));
+        }
+        setGrandFatherVector();
+    }
+    private void setGrandFatherVector(){
+        for(int i = 0; i < GrandFatherVector.size(); i++){
+            GrandFatherVector.set(i, FatherVector.get(FatherVector.get(i)));
+        }
+    }
+    private void shortCutting(){
+        TotOperations += GrandFatherVector.size();
+        OperationsInParallel += 1;
+        for(int i = 0; i < GrandFatherVector.size(); i++){
+            NextFatherVector.set(i, GrandFatherVector.get(i));
+        }     
+    }
+    private void fnextValidAssign(Integer fnextStatus, Integer toBeAssign){
+        if(fnextStatus > toBeAssign){
+            NewFatherVector.set(fnextStatus, toBeAssign);
+        }
+    }
+    private void OriginslTreeHooking(){
+        for(int i = 0; i < EdgeStartPoint.size(); i++){
+            if(FatherVector.get(EdgeStartPoint.get(i)) == GrandFatherVector.get(EdgeStartPoint.get(i))){
+                fnextValidAssign(FatherVector.get(EdgeStartPoint.get(i)), FatherVector.get(EdgeEndPoint.get(i)));
+            }
+        }
+        updateFatherVector();
+    }
+    private void StochasticHookingAndAggressiveHooking(){
+        for(int i = 0; i < EdgeStartPoint.size(); i++){
+            fnextValidAssign(FatherVector.get(EdgeStartPoint.get(i)), GrandFatherVector.get(EdgeEndPoint.get(i)));
+        }
+        for(int i = 0; i < EdgeStartPoint.size(); i++){
+            fnextValidAssign(EdgeStartPoint.get(i), GrandFatherVector.get(EdgeEndPoint.get(i)));
+        }        
+        updateFatherVector();
+    }
+    SVKernel(int NumOfElements, ArrayList<Integer> Start, ArrayList<Integer> End){
+        FatherVector = new ArrayList<Integer>(NumOfElements);
+        NextFatherVector = new ArrayList<Integer>(NumOfElements);
+        GrandFatherVector = new ArrayList<Integer>(NumOfElements);
+        TotOperations = 0;
+        OperationsInParallel = 0;
+        for (int i = 0; i < NumOfElements; i++){
+            FatherVector.set(i, i);
+            NextFatherVector.set(i, i);
+            GrandFatherVector.set(i, i);
+        }
+        EdgeStartPoint = Start;
+        EdgeEndPoint = End;
+    }
+    public void print(){
+        System.out.println("Total operations: " + TotOperations);
+        System.out.println("Operations in parallel computing: " + OperationsInParallel);
+    }
+
+}
