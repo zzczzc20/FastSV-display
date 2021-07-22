53176b29236c98551b590594c3a047fb20839c16
zzczzc20 <62371873+zzczzc20@users.noreply.github.com>
2021-07-22 11:41:20 +0800

    Add BruteForce Algorithm

--- SV.java
+++ SV.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class SV {
     public static void main(String args[]){
         System.out.println("This is the display program for FastSV");
-        int[] ES = {0, 1, 2, 3, 4, 5, 3, 2};
-        int[] EE = {1, 0, 3, 2, 2, 3, 5, 4};
+        int[] ES = {0, 1, 2, 3, 4, 5, 3, 2, 4, 1};
+        int[] EE = {1, 2, 3, 4, 5, 4, 2, 1, 3, 0};
         ArrayList<Integer> ESAL = new ArrayList<Integer>();
         ArrayList<Integer> EEAL = new ArrayList<Integer>();
         for(int i = 0; i < ES.length; i++){
@@ -12,14 +12,19 @@ public class SV {
         }
         SVKernel FastSV = new SVKernel(6, ESAL, EEAL);
         SVKernel SlowSV = new SVKernel(6, ESAL, EEAL);
+        SVKernel BFSV = new SVKernel(6, ESAL, EEAL);
         FastSV.FastSV();
         SlowSV.OriginalSV();
+        BFSV.BruteForce();
         System.out.println("This is FastSV: ");
         FastSV.printVector();
         FastSV.print();
         System.out.println("This is SlowSV: ");
         SlowSV.printVector();
         SlowSV.print();
+        System.out.println("This is BruteForce: ");
+        BFSV.printVector(true);
+        BFSV.print();
     }
 }
 
@@ -30,6 +35,8 @@ class SVKernel {
     private ArrayList<Integer> FatherVectorBuffer;
     private ArrayList<Integer> NextFatherVector;
     private ArrayList<Integer> GrandFatherVector;
+    private ArrayList<Integer> BruteForceVector;
+    private ArrayList<Integer> BruteForceVectorBuffer;
     private ArrayList<Integer> EdgeStartPoint;
     private ArrayList<Integer> EdgeEndPoint;
     private boolean compareTwoArrayList(ArrayList<Integer> a, ArrayList<Integer> b) {
@@ -43,6 +50,13 @@ class SVKernel {
     private void memcpy(){
         for(int i = 0; i < FatherVector.size(); i++){
             FatherVectorBuffer.set(i, FatherVector.get(i));
+        }
+    }
+    private void memcpy(boolean choice){
+        if(choice){
+            for(int i = 0; i < FatherVector.size(); i++){
+                BruteForceVectorBuffer.set(i, BruteForceVector.get(i));
+            }
         }
     }
     private void updateFatherVector(){
@@ -98,6 +112,8 @@ class SVKernel {
         FatherVectorBuffer = new ArrayList<Integer>();
         NextFatherVector = new ArrayList<Integer>();
         GrandFatherVector = new ArrayList<Integer>();
+        BruteForceVector = new ArrayList<Integer>();
+        BruteForceVectorBuffer = new ArrayList<Integer>();
         TotOperations = 0;
         OperationsInParallel = 0;
         for (int i = 0; i < NumOfElements; i++){
@@ -105,6 +121,8 @@ class SVKernel {
             NextFatherVector.add(i);
             GrandFatherVector.add(i);
             FatherVectorBuffer.add(i);
+            BruteForceVector.add(i);
+            BruteForceVectorBuffer.add(i);
         }
         EdgeStartPoint = Start;
         EdgeEndPoint = End;
@@ -129,6 +147,17 @@ class SVKernel {
         }
         System.out.println(s);
     }
+    public void printVector(boolean choice){
+        if(choice){
+            System.out.println("BruteForceVector: ");
+            String s = "";
+            for(int i = 0; i < FatherVector.size(); i++){
+                s += BruteForceVector.get(i);
+                s += " ";
+            }
+            System.out.println(s);
+        }
+    }
     public void OriginalSV(){
         int i = 0;
         while (true) {
@@ -151,6 +180,45 @@ class SVKernel {
             }
             memcpy();
             i++;
+        }
+    }
+    public void BruteForce(){
+        int i = 0;
+        while (true) {
+            for (int iter = 0; iter < EdgeStartPoint.size(); iter++){
+                TotOperations += 3; // 2 get + 1 compare
+                OperationsInParallel += 3;
+                if(EdgeStartPoint.get(iter) == EdgeEndPoint.get(iter)){
+                    continue;
+                }
+                else{
+                    Integer small = min(EdgeStartPoint.get(iter), EdgeEndPoint.get(iter));
+                    Integer big = max(EdgeStartPoint.get(iter), EdgeEndPoint.get(iter));
+                    replace(EdgeStartPoint, big, small);
+                    replace(EdgeEndPoint, big, small);
+                    replace(BruteForceVector, big, small);
+                }
+            }
+            if(i != 0){
+                if(compareTwoArrayList(BruteForceVector, BruteForceVectorBuffer)) break;
+            }
+            memcpy(true);
+            i++;
+        }
+    }
+    private Integer min(Integer a, Integer b){
+        return (a < b) ? a : b;
+    }
+    private Integer max(Integer a, Integer b){
+        return (a > b) ? a : b;
+    }
+    private void replace(ArrayList<Integer> collection, Integer tobereplaced, Integer value){
+        TotOperations += collection.size() * 3;
+        OperationsInParallel += 3;
+        for(int i = 0; i < collection.size(); i++){
+            if(collection.get(i) == tobereplaced){
+                collection.set(i, value);
+            }
         }
     }
 }
